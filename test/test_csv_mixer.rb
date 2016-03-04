bd388e41bc6779b7b57711e68d3ee92212e485f3
Miker Renardus <miker.renardus@gmail.com>
2016-03-04 10:59:02 +0100

    Remaining hola's changed to csv_mixer

--- test/test_csv_mixer.rb
+++ test/test_csv_mixer.rb
@@ -3,14 +3,14 @@ require 'csv_mixer'
 
 class CsvMixerTest < Test::Unit::TestCase
   def test_english_hello
-    assert_equal "hello world", Hola.hi("english")
+    assert_equal "hello world", CsvMixer.hi("english")
   end
 
   def test_any_hello
-    assert_equal "hello world", Hola.hi("ruby")
+    assert_equal "hello world", CsvMixer.hi("ruby")
   end
 
   def test_spanish_hello
-    assert_equal "hola mundo", Hola.hi("spanish")
+    assert_equal "hola mundo", CsvMixer.hi("spanish")
   end
 end
